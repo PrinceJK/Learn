3c972fec160be1624784c5e1953aadd7af1cca76
John Kehinde <kehindejohn018@gmail.com>
2022-09-17 06:22:13 +0100

    my first commit

---
+++ LearnGit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LearnGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalulateFigures(3, 4));
+        }
+
+        public static int CalulateFigures(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
